623d81e28967b8b5b274b05b1bc57c1b4aec57d2
solaireastora125 <solaireastora125@gmail.com>
2022-12-18 02:52:19 +0800

    init commint

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
-def my_app():
-    return '<h1>Agriculture Monitoring System</h1>'
-    
-@app.route('/sensor')
-def my_app():
-    return 'First Flask application!'
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+if __name__ == '__main__':
+   app.run(debug = True)
