c259a9a7458075bb1ebe33b1b7163e61a9b73e07
solaireastora125 <solaireastora125@gmail.com>
2022-12-21 12:14:21 +0800

    update changes

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask, redirect, url_for, request
-app = Flask(__name__)
+from flask import Flask, redirect, url_for, request, render_template
 
+app = Flask(__name__,template_folder='templates',static_folder='static')
+
+@app.route('/')
+def home():
+	return render_template('login.html')
+   
 @app.route('/success/<name>')
 def success(name):
-   return 'welcome %s' % name
+	return render_template('index.html', p1=name)
+   #return 'welcome %s' % name
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
+      name = request.form['nm']
+      pswd = request.form['pw']
+      return redirect(url_for('success',name = name))
    else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+      username = request.args.get('nm')
+      return redirect(url_for('success',name = name))
 
 if __name__ == '__main__':
    app.run(debug = True)
+
