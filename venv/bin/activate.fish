ed4b86599b68abfaec4af31497d81359cae0ad69
SolaireAstora125 <solaireastora125@gmail.com>
2023-01-01 11:12:47 +0800

    initiate commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ziyad/IoT-Project/venv"
+set -gx VIRTUAL_ENV "/home/ziyad/Desktop/IoT-Project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
