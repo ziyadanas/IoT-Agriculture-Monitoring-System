73aed713532a7fd0988ee16a0619d3f06825e66f
SolaireAstora125 <solaireastora125@gmail.com>
2023-01-03 14:10:45 +0800

    initiate commit

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 from flask import Flask, redirect, url_for, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_app import db
 import os
 
 
+
 app = Flask(__name__, template_folder='templates')
+
+
 sm	= 0
 ldr	= 0
 
 #PostgreSQL DB config----------------------------------------------
+app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="mohdafiqazizi",
+    password="saya0000",
+    hostname="mohdafiqazizi.mysql.pythonanywhere-services.com",
+    databasename="mohdafiqazizi$agriculture",
+    )
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class data(db.Model):
+    __tablename__ = "data"
+    id 	= db.Column(db.Integer, primary_key=True)
+    sm 	= db.Column(db.String(4096))
+    ldr = db.Column(db.String(4096))
+    def __init__(self,sm,ldr):
+    self.sm 	= sm
+    self.ldr	= ldr
 
 # Backend Web-------------------------------------------------------
 @app.route('/')
