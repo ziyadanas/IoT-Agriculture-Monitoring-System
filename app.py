e6e537d13d71180854a7012eae6e90a502711cdc
SolaireAstora125 <solaireastora125@gmail.com>
2023-01-03 15:40:49 +0800

    initiate commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
 import os
 
 app = Flask(__name__, template_folder='templates')
@@ -9,12 +10,21 @@ ldr	= 0
 
 #PostgreSQL DB config----------------------------------------------
 app.config["DEBUG"] = True
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="mohdafiqazizi",
-    password="saya0000",
-    hostname="mohdafiqazizi.mysql.pythonanywhere-services.com",
-    databasename="mohdafiqazizi$agriculture",
+SQLALCHEMY_DATABASE_URI = "postgresql://{username}:{password}@{hostname}/{databasename}".format(
+    username="agriculturedb_06pt_user",
+    password="xWlYDxSmhulErXEGgNLF6M23fR1IYl0G",
+    hostname="dpg-cepti91a6gdgsmcgg3c0-a.singapore-postgres.render.com",
+    databasename="agriculturedb_06pt",
     )
+
+"""
+#external database
+postgres://agriculturedb_06pt_user:xWlYDxSmhulErXEGgNLF6M23fR1IYl0G@dpg-cepti91a6gdgsmcgg3c0-a.singapore-postgres.render.com/agriculturedb_06pt
+
+#internal database    
+postgres://agriculturedb_06pt_user:xWlYDxSmhulErXEGgNLF6M23fR1IYl0G@dpg-cepti91a6gdgsmcgg3c0-a/agriculturedb_06pt
+"""
+
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -29,6 +39,17 @@ class data(db.Model):
 #	def __init__(self,sm,ldr):
 #	self.sm 	= sm
 #	self.ldr	= ldr
+
+# Initialize DB manually--------------------------------------------
+engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sa.inspect(engine)
+if not inspector.has_table("users"):
+	with app.app_context():
+		db.drop_all()
+		db.create_all()
+		app.logger.info('Initialized the database!')
+else:
+	app.logger.info('Database already contains the users table.')
 
 # Backend Web-------------------------------------------------------
 @app.route('/')
