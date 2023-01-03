46bb1d99ead3ac29d2875964768617c8e74a1f59
SolaireAstora125 <solaireastora125@gmail.com>
2023-01-03 13:53:55 +0800

    initiate commit

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask, redirect, url_for, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-import logging
 import os
-from logging.handlers import RotatingFileHandler
-import sqlalchemy as sa
-from click import echo
-from flask.logging import default_handler
-#from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, template_folder='templates')
@@ -15,67 +8,6 @@ sm	= 0
 ldr	= 0
 
 #PostgreSQL DB config----------------------------------------------
-
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    FLASK_ENV = 'development'
-    DEBUG = False
-    TESTING = False
-    SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
-    # Since SQLAlchemy 1.4.x has removed support for the 'postgres://' URI scheme,
-    # update the URI to the postgres database to use the supported 'postgresql://' scheme
-    if os.getenv('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-    else:
-        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Logging
-    LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
-
-class ProductionConfig(Config):
-    FLASK_ENV = 'production'
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI', default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
-    WTF_CSRF_ENABLED = False
-
-def configure_logging(app):
-    # Logging Configuration
-    if app.config['LOG_WITH_GUNICORN']:
-        gunicorn_error_logger = logging.getLogger('gunicorn.error')
-        app.logger.handlers.extend(gunicorn_error_logger.handlers)
-        app.logger.setLevel(logging.DEBUG)
-    else:
-        file_handler = RotatingFileHandler('instance/flask-user-management.log',
-                                           maxBytes=16384,
-                                           backupCount=20)
-        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
-        file_handler.setFormatter(file_formatter)
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
-    # Remove the default logger configured by Flask
-    app.logger.removeHandler(default_handler)
-
-    app.logger.info('Starting the Flask User Management App...')
-
-# Check if the database needs to be initialized
-engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-inspector = sa.inspect(engine)
-if not inspector.has_table("users"):
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-        app.logger.info('Initialized the database!')
-else:
-    app.logger.info('Database already contains the users table.')
-
 
 # Backend Web-------------------------------------------------------
 @app.route('/')
