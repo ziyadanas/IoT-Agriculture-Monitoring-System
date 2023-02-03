581845a8171c3573804046a5e4ea28848929625b
SolaireAstora125 <solaireastora125@gmail.com>
2023-02-03 17:16:49 +0800

    initiate commit

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ import os
 
 app = Flask(__name__, template_folder='templates')
 
+sm	= 0
+ldr	= 0
+t	= 0
+
 #PostgreSQL DB config----------------------------------------------
 app.config["DEBUG"] = True
 SQLALCHEMY_DATABASE_URI = "postgresql://{username}:{password}@{hostname}/{databasename}".format(
@@ -31,13 +35,15 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-
 class data(db.Model):
-	__tablename__ = "data"
-	id 			= db.Column(db.Integer, primary_key=True)
-	sm 			= db.Column(db.Integer)
-	ldr 		= db.Column(db.Integer)
-	timestamp	= db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    __tablename__ = "data"
+    id 	= db.Column(db.Integer, primary_key=True)
+    sm 	= db.Column(db.Integer)
+    ldr = db.Column(db.Integer)
+    t	= db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#	def __init__(self,sm,ldr):
+#	self.sm 	= sm
+#	self.ldr	= ldr
 
 # Initialize DB manually--------------------------------------------
 engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
@@ -50,7 +56,6 @@ if not inspector.has_table("users"):
 else:
 	app.logger.info('Database already contains the users table.')
 
-
 # Backend Web-------------------------------------------------------
 @app.route('/')
 def home():
@@ -58,16 +63,23 @@ def home():
 
 @app.route('/sensor', methods = ['POST', 'GET'])
 def sensor():
-	global sm, ldr, timestamp
+	global sm
+	global ldr
+	global t
 	if request.method == 'POST':
-		sm			= request.form.get('sm')
-		ldr			= request.form.get('ldr')
-		timestamp	= datetime.now(tz=timezone('Asia/Kuala_Lumpur'))
-		data = data(sm=sm,ldr=ldr,timestamp=timestamp)
-		db.session.add(data)
+		sm = request.form.get('sm')
+		ldr = request.form.get('ldr')
+		datadb = data(
+		sm	= request.form.get('sm'),
+		ldr	= request.form.get('ldr'),
+		t	= datetime.now(tz=timezone('Asia/Kuala_Lumpur'))
+		)
+		db.session.add(datadb)
 		db.session.commit()
 	return render_template('sensor.html', sm=sm, ldr=ldr)
 		
+#	else:
+#		return "<h2>ERROR</h2>"
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)# Get port number of env at runtime, else use default port 5000
